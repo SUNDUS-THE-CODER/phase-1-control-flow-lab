53c27871f13f7871e8c3ab2979a3cf67334745f5
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 14:46:49 +0000

    add all requied functions

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride > 2500) return 'No can do.'
+  let charge = '';
+  ride <= 400 ? charge = 'This one is on me!' 
+    : ride > 400 && ride < 2000 ? charge = 'That will be twenty bucks.' 
+      : ride > 2000 && ride <= 2500 ? charge = 'I will gladly take your thirty bucks.' : null;
+  return charge;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') return 'Ok, sounds good.';
+  if (city !== 'NYC') return 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') return 'Thank you so much.'
+  if (tip === 'not as generous') return 'Thank you.'
+  return 'Bye.'
 }
